6c5af091541eba61c2f4a3a4582bafcde5faccbf
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2026-04-06 12:42:43 +0200

    feat: version and name of plugin

--- src/configs/recommended.ts
+++ src/configs/recommended.ts
@@ -1,9 +1,17 @@
 import type { Linter } from 'eslint'
 import vueParser from 'vue-eslint-parser'
+import noFoo from '../rules/no-foo.js'
 
 export const recommended: Linter.Config = {
   name: 'fluent-vue/recommended',
-  files: ['**/*.{js,mjs,cjs,ts,mts,cts,vue}'],
+  files: ['**/*.vue'],
+  plugins: {
+    'fluent-vue': {
+      rules: {
+        'no-foo': noFoo,
+      },
+    },
+  },
   languageOptions: {
     parser: vueParser,
     parserOptions: {
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
+import type { Rule } from 'eslint'
+import { ESLint } from 'eslint'
+import { readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 import noFoo from './rules/no-foo.js'
 import { configs } from './configs/index.js'
 
-export const rules = {
+const pkg = JSON.parse(
+  readFileSync(join(dirname(fileURLToPath(import.meta.url)), '..', 'package.json'), 'utf-8'),
+)
+
+export const rules: Record<string, Rule.RuleModule> = {
   'no-foo': noFoo,
 }
 
-export default {
+const plugin: ESLint.Plugin = {
+  meta: {
+    name: pkg.name,
+    version: pkg.version,
+    namespace: 'fluent-vue',
+  },
   rules,
   configs,
 }
+
+export default plugin
